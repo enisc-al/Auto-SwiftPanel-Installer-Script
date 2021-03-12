0cded246d4168ad8625a53f66748a0a4fac49555
enisc-al <80548505+enisc-al@users.noreply.github.com>
2021-03-12 22:12:03 +0100

    Create index.php

---
+++ tmp/admin/javascript/index.php
@@ -0,0 +1,15 @@
+<?php //004d4
+// WARNING: This script is protected. Any attempt to reverse engineer, debug or de-code this file or its dependent files is strictly prohibited.
+// @package SWIFT Panel Version 1.6.1
+// @Copyright (C) 2008-2009 SWIFT Panel All rights reserved
+// @link http://www.swiftpanel.com
+// @date 2010-8-14
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');@dl($__ln);if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}@dl($__ln);}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the site administrator.');exit(199);
+?>
+4+oV5BBX0PX0D/hV6LkKkp0VArKrbLM367YgOyu9DlFO4WbsdDipxro4jdhj4Q9Y95XAE5YrxVi/
+GdTBj5tbmT49Fp/w70dzyYj3gqb6MhI8uQA5i2vntT+zm4ck3UEylNaKynovMy04HxkEipg4jZOI
+0a5iW6/wNwK4nHgvcar6hk5ItezLrRiY6AL0TFL06TT5R3Ir+FwsJmovG4+mZVx4nf3diWNyoQes
+Ul+ajd4Tfxxz2HAax7kb30TaYvxvgnI218OGeAL3qeagBkHxGVW1KoERR1DPqX4Y7XbfOAWwRWQf
+ezjEpAP5DAPtYH00/vFAEIY2lso6H/U4qcrhxGM13QaHO6F1pnIChpMcc7XQVIT8rMYdnQsz+nVf
+2bIgN4AHN+C+2QM7ExtCueoWrtzZBFkrzOp9yXLerAM2hUgf17iUOpXyl8CewytELxAlrEp1iYfA
+6SsMeMkVXH7P24SipO4gS2MOtDXI55UxFVbovLOenX1CgDr6a5S=
